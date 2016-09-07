83663d23d5eb130c5d81d3d55d7f729159db0c48
1vasari <me@1vasari.xyz>
2016-09-07 21:04:19 +1000

    Install bower dependencies on deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,6 +82,22 @@ namespace :deploy do
   after  :finishing,    :restart
 end
 
+namespace :bower do
+  desc 'Install bower dependencies'
+
+  task :install do
+    on roles(:web) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'bower:install CI=true'
+        end
+      end
+    end
+  end
+end
+
+before 'deploy:compile_assets', 'bower:install'
+
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
