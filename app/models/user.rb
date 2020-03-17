974a2d82559d8bbb9dce2b9911ade5f2f1664da0
Nathan McCallum <nathan@1vas.me>
2020-03-17 13:55:03 +1000

    Remove email-related Devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,8 @@ class User < ActiveRecord::Base
   devise(
     :database_authenticatable,
     :registerable,
-    :recoverable,
     :rememberable,
     :validatable,
-    :confirmable,
-    :lockable,
     :trackable
   )
 
