55f43a1a60ce555fbcede3595d9123cdd9ccf91e
Nathan McCallum <nathan@1vas.me>
2020-05-23 13:42:45 +1000

    Fix condition

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,19 +4,12 @@ class ApiController < ApplicationController
   def transpose_song
     song = current_user.songs.find params[:song_id]
 
-    if !song.nil?
-      compiler = SongdownCompiler.new(
-        :input => song.content,
-        :key => song.key
-      )
+    compiler = SongdownCompiler.new(
+      :input => song.content,
+      :key => song.key
+    )
 
-      compiler.change_key params[:new_key]
-
-      render :json => {
-        :transposed_song => compiler.to_html
-      }
-    else
-      # TODO
-    end
+    compiler.change_key params[:new_key]
+    render :json => { :transposed_song => compiler.to_html }
   end
 end
