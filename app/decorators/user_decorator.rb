4fecbc657edae1443e8f4f2e5516bc7663f7dd52
Nathan McCallum <hey@nathan-mccallum.com>
2021-03-06 11:06:44 +1000

    Improve show email when there's no first/last name or username

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,8 +4,12 @@ class UserDecorator
     if user.first_name.present?
       buffer << user.first_name
       buffer << user.last_name if user.last_name.present?
+      buffer << "(#{user.username})" if user.username.present?
+    elsif user.username.present?
+      buffer << user.username
+    else
+      buffer << user.email
     end
-    buffer << (buffer.empty? ? user.username : "(#{user.username})") if user.username.present?
     buffer.join ' '
   end
 end
